7a08785064cc3016c675c85f90d2b8b3030e4f62
Reisui <87981144+namnaowna@users.noreply.github.com>
2024-03-08 23:07:50 +0700

    component Listcamp

---
+++ src/components/Camplist.vue
@@ -0,0 +1,25 @@
+<script setup>
+import CampCard from "./CampCard.vue"
+import ListCard from "./ListCard.vue"
+import camp from "../../data/camp.json"
+</script>
+
+<template>
+  <div>
+    <ListCard :items="camp">
+      <template #default="slotProps">
+        <CampCard>
+          <template v-slot:location>
+            <img :src="slotProps.item.ListImage[0].image" alt="location" />
+          </template>
+          <template v-slot:title>{{ slotProps.item.Title }}</template>
+          <template v-slot:description>{{
+            slotProps.item.Description
+          }}</template>
+        </CampCard>
+      </template>
+    </ListCard>
+  </div>
+</template>
+
+<style scoped></style>
