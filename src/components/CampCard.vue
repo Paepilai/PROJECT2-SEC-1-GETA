586bd1161e4a0eb2332e6085b76784fc9c491593
findmelily <pinny.pinngam@gmail.com>
2024-03-17 16:14:04 +0700

    Demo card ui

--- src/components/CampCard.vue
+++ src/components/CampCard.vue
@@ -1,13 +1,10 @@
 <template>
-  <div class="border border-gray-200 rounded p-4">
+  <div class="border border-gray-200 rounded p-4 flex-col">
     <slot name="location"></slot>
-    <h3 class="text-lg font-semibold">{{ name }}</h3>
-    <p class="text-gray-600">{{ location }}</p>
-    <router-link
-      :to="{ name: 'CampDetail', params: { id: id } }"
-      class="mt-2 block bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
-      >Explore Camp</router-link
-    >
+    <h3 class="text-lg font-semibold text-center">{{ name }}</h3>
+    <p class="text-gray-600 text-center">{{ location }}</p>
+    <router-link :to="{ name: 'CampDetail', params: { id: id } }"
+      class="btn bg-[#F79C1D] hover:bg-[#ca7f41] text-white text-center">Explore Camp</router-link>
   </div>
 </template>
 
