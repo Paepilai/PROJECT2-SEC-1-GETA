60931cba1e38b6e35d0d938f6fd8f78c0ab8f993
Reisui <87981144+namnaowna@users.noreply.github.com>
2024-03-17 16:14:49 +0700

    Update CampCard.vue structure

--- src/components/CampCard.vue
+++ src/components/CampCard.vue
@@ -1,13 +1,19 @@
 <template>
-  <div class="border border-gray-200 rounded p-4">
-    <slot name="location"></slot>
-    <h3 class="text-lg font-semibold">{{ name }}</h3>
-    <p class="text-gray-600">{{ location }}</p>
-    <router-link
-      :to="{ name: 'CampDetail', params: { id: id } }"
-      class="mt-2 block bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
-      >Explore Camp</router-link
-    >
+  <div class="card w-96 h-full bg-base-100 shadow-xl p-5 border">
+    <div class="flex justify-center">
+      <slot name="location"></slot>
+    </div>
+    <div class="pl-2">
+      <h3 class="text-lg font-semibold pt-3">📍{{ name }}</h3>
+      <p class="text-gray-600">{{ location }}</p>
+    </div>
+    <div class="card-actions justify-end">
+      <router-link
+        :to="{ name: 'CampDetail', params: { id: id } }"
+        class="mt-5 btn bg-[#F79C1D] hover:bg-[#F79C1D] hover:ring-2 ring-[#B99B9B] active:bg-[#B99B9B] focus:outline-none"
+        >Explore Camp</router-link
+      >
+    </div>
   </div>
 </template>
 
@@ -16,5 +22,5 @@ const { title, location } = defineProps({
   name: String,
   location: String,
   id: Number,
-});
+})
 </script>
