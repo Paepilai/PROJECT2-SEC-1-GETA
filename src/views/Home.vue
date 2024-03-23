faf09d18c507e83cc9009657f463130f13242507
findmelily <pinny.pinngam@gmail.com>
2024-03-23 15:03:54 +0700

    Remove list of name in search

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@ import campData from "../../data/camp.json";
 import Camplist from "../components/Navbar.vue";
 import CampCard from "../components/CampCard.vue";
 import ListCard from "../components/ListCard.vue";
-import { ref, onMounted, computed} from "vue";
+import { ref, onMounted, computed } from "vue";
 
 const campgrounds = ref([]);
 
@@ -35,33 +35,20 @@ onMounted(async () => {
     <div class="flex items-center justify-between px-3 py-5">
       <h1 class="ml-8 text-4xl font-bold">Hi [Name], welcome to your space!</h1>
       <div id="app">
-    <label class="mr-8 input input-bordered- flex items-center gap-2">
-      <input type="text" v-model="searchQuery" class="grow" placeholder="Search" />
-    </label>
-    <ul v-if="searchQuery !== ''">
-      <li v-for="item in filteredItems" :key="item.id">
-        {{ item.name }}
-      </li>
-    </ul>
-  </div>
+        <label class="mr-8 input input-bordered- flex items-center gap-2">
+          <input type="text" v-model="searchQuery" class="grow" placeholder="Search" />
+        </label>
+      </div>
     </div>
     <h1 class="ml-9 text-3xl font-bold">Campgrounds</h1>
   </div>
   <div>
     <ListCard :items="filteredItems">
       <template #default="slotProps">
-        <CampCard
-          :name="slotProps.item.name"
-          :location="slotProps.item.location"
-          :id="slotProps.item.id"
-        >
+        <CampCard :name="slotProps.item.name" :location="slotProps.item.location" :id="slotProps.item.id">
           :
           <template v-slot:location>
-            <img
-              :src="slotProps.item.image"
-              alt="Location Image"
-              class="h-60 w-80 object-cover rounded-t"
-            />
+            <img :src="slotProps.item.image" alt="Location Image" class="h-60 w-80 object-cover rounded-t" />
           </template>
         </CampCard>
       </template>
