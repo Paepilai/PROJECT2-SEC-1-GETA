ae8554480891fe7f92f46a715fb971dba6d2ebaf
jakathornsorv <111364247+jakathornsorv@users.noreply.github.com>
2024-03-30 02:54:56 +0700

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,37 +1,49 @@
 <script setup>
-import { RouterLink } from "vue-router"
+import { ref } from 'vue'
 
+const users = [
+  {
+    id: "1",
+    name: "John Doe",
+    email: "johndoe@example.com",
+    password: "password",
+    bio: "When I'm not developing software, you can find me exploring the great outdoors and camping under the stars.",
+    favorite: []
+  },
+
+]
+
+const loginUser = () => {
+
+  const user = users.find(user => user.email === username.value && user.password === password.value)
+  if (user) {
+    router.push('/home')
+  } else {
+    alert('Invalid username or password')
+  }
+}
 </script>
 
 <template>
     <div class="full-page">
-        <div class="box-center">
-            <div class="mt-5 text-2xl font-bold text-slate-950">Login</div>
-            <div class="m-5">
-                <label class="input input-bordered flex items-center gap-2 m-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
-                        class="w-4 h-4 opacity-70">
-                        <path
-                            d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-                    </svg>
-                    <input type="text" class="grow" placeholder="Username" />
-                </label>
-
-
-                <label class="input input-bordered flex items-center gap-2 m-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
-                        class="w-4 h-4 opacity-70">
-                        <path fill-rule="evenodd"
-                            d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-                            clip-rule="evenodd" />
-                    </svg>
-                    <input type="password" class="grow" placeholder="Password" />
-                </label>
-                <RouterLink to="/home" class="btn btn-outline btn-accent"> Login </RouterLink>
-            </div>
+      <div class="box-center">
+        <div class="mt-5 text-2xl font-bold text-slate-950">Login</div>
+        <div class="m-5">
+          <label class="input input-bordered flex items-center gap-2 m-2">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 opacity-70">
+            </svg>
+            <input type="text" class="grow" v-model="username" placeholder="Username" />
+          </label>
+          <label class="input input-bordered flex items-center gap-2 m-2">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 opacity-70">
+            </svg>
+            <input type="password" class="grow" v-model="password" placeholder="Password" />
+          </label>
+          <button class="btn btn-outline btn-accent" @click="loginUser">Login</button>
         </div>
+      </div>
     </div>
-</template>
+  </template>
 
 <style scoped>
 .full-page {
