bab03a75c4cd8a868646c82d56475318fbaa9070
Reisui <87981144+namnaowna@users.noreply.github.com>
2024-03-30 18:06:02 +0700

    Update Favorite

--- src/stores/users.js
+++ src/stores/users.js
@@ -24,18 +24,12 @@ const myUserTodo = defineStore("usertodo", () => {
     })
   }
 
-  const removeTodo = (removeId) => {
-    usertodo.value.splice(
-      usertodo.value.findIndex((todo) => todo.id === removeId),
-      1
-    )
-  }
   //views
   const getTodos = () => {
     return usertodo.value
   }
 
-  return { addTodo, addTodos, updateTodo, removeTodo, getTodos }
+  return { addTodo, addTodos, updateTodo, getTodos }
 })
 
 export { myUserTodo }
