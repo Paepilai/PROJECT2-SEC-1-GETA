801c1a2233f7ecf8b41f8edb6ffd9c81450cb005
findmelily <pinny.pinngam@gmail.com>
2024-03-30 23:53:45 +0700

    Update Navbar

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,11 @@
-
-import { createRouter, createWebHistory } from "vue-router"
-import Home from "../views/Home.vue"
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
 import Profile from "../views/Profile.vue";
-import CampDetail from "../views/CampDetails.vue"
-import Booking from "../views/Booking.vue"
-import AvailableArea from "../views/AvailableArea.vue"
-import Receipt from "../views/Receipt.vue"
-import MyBooking from "../views/MyBooking.vue"
-
+import CampDetail from "../views/CampDetails.vue";
+import Booking from "../views/Booking.vue";
+import AvailableArea from "../views/AvailableArea.vue";
+import Receipt from "../views/Receipt.vue";
+import MyBooking from "../views/MyBooking.vue";
 
 export const routes = [
   {
@@ -21,12 +19,11 @@ export const routes = [
     component: CampDetail,
   },
   {
-
     path: "/profile",
     name: "Profile",
     component: Profile,
   },
-];
+  {
     path: "/booking",
     name: "Booking",
     component: Booking,
@@ -47,12 +44,11 @@ export const routes = [
     name: "MyBooking",
     component: MyBooking,
   },
-]
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-
 });
 
 export default router;
