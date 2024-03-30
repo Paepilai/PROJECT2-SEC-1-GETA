885abeb4810e35894c498f5cfa8e6b565e3f97cb
findmelily <pinny.pinngam@gmail.com>
2024-03-31 00:05:04 +0700

    Update CampDetails

--- src/views/CampDetails.vue
+++ src/views/CampDetails.vue
@@ -8,10 +8,7 @@
           <h1>{{ campground.name }}</h1>
         </div>
         <div>
-          <img
-            :src="campground.image"
-            class="rounded-lg max-w-lg justify-center mx-auto"
-          />
+          <img :src="campground.image" class="rounded-lg max-w-lg justify-center mx-auto" />
         </div>
       </div>
     </div>
@@ -31,11 +28,7 @@
 
         <div class="text-lg m-10 pl-20 pr-20 pb-10">
           <template v-if="Array.isArray(campground.rule)">
-            <ul
-              v-for="(rule, index) in campground.rule"
-              :key="index"
-              class="list-disc pl-10"
-            >
+            <ul v-for="(rule, index) in campground.rule" :key="index" class="list-disc pl-10">
               <li>{{ rule }}</li>
             </ul>
           </template>
@@ -65,11 +58,7 @@
           <h3>Service</h3>
         </div>
         <div class="text-xl pl-10 pr-10 pb-5">
-          <ul
-            v-for="(service, index) in campground.service"
-            :key="index"
-            class="list-disc pl-10"
-          >
+          <ul v-for="(service, index) in campground.service" :key="index" class="list-disc pl-10">
             <li>{{ service }}</li>
           </ul>
         </div>
@@ -79,11 +68,7 @@
           <h3>Price</h3>
         </div>
         <div class="text-xl pl-10 pr-10 pb-5">
-          <ul
-            v-for="(price, index) in campground.price"
-            :key="index"
-            class="list-disc pl-10"
-          >
+          <ul v-for="(price, index) in campground.price" :key="index" class="list-disc pl-10">
             <li>
               {{ price }}
             </li>
@@ -96,10 +81,8 @@
             class="btn bg-[#F79C1D] hover:bg-[#F79C1D] text-white text-2xl"
             >Join Camp</RouterLink
           > -->
-          <RouterLink
-            :to="{ path: '/available', query: { campId: campground.id } }"
-            class="btn bg-[#F79C1D] hover:bg-[#F79C1D] text-white text-2xl"
-          >
+          <RouterLink :to="{ path: '/available', query: { campId: campground.id } }"
+            class="btn bg-[#F79C1D] hover:bg-[#F79C1D] text-white text-2xl">
             Join Camp
           </RouterLink>
         </div>
@@ -116,15 +99,19 @@
 <script setup>
 import { ref, onMounted } from "vue"
 import { useRoute } from "vue-router"
-import campData from "../../data/camp.json"
+import Data from "../../data/camp.json"
 
 const route = useRoute()
 const campground = ref(null)
+// เรียก camp จาก data เพราะมี 2 ข้อมูลตอนนี้
+const campData = ref(Data.camp)
 
+//เหมือนว่าเช็คไอดีถูกแล้วแต่มันไม่ขึ้นข้อมูลแคมป์น่ะ ช่วยดูให้หน่อย
 onMounted(() => {
   const id = route.params.id
-  campground.value = campData.find((camp) => camp.id === parseInt(id))
+  campground.value = Data.camp.find((camp) => camp.id === parseInt(id))
 })
+
 </script>
 
 <style scoped></style>
