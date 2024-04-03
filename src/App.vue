0aabb96e230107c880d5dbb0027bbcb114b83201
Reisui <87981144+namnaowna@users.noreply.github.com>
2024-04-04 00:48:15 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,6 @@ import { onMounted, ref } from "vue"
 import { getItems } from "./libs/fetchUtils.js"
 import { myUserTodo } from "./stores/users.js"
 
-const myUser = myUserTodo()
-onMounted(async () => {
-  //users
-  const items = await getItems(`${import.meta.env.VITE_USER_BASE1_URL}/1`)
-  myUser.addTodos([items])
-})
-
 const darkMode = ref(false)
 const toggleDarkMode = () => {
   darkMode.value = !darkMode.value
