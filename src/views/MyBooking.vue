0603971a4ec9e31e029b0c02bfd843d9e7e099a3
MookizsHub <mookies2004@gmail.com>
2024-04-03 14:20:15 +0700

    update mybooking with image

--- src/views/MyBooking.vue
+++ src/views/MyBooking.vue
@@ -1,22 +1,25 @@
 <template>
-  <div class="max-w-2xl mx-auto mt-10 bg-white rounded-lg overflow-hidden">
+  <div>
     <div v-if="isLoading">Loading...</div>
     <div v-else>
       <div v-for="(booking, index) in bookings" :key="index" class="card">
         <h2>Booking Number: {{ booking.id }}</h2>
+        <p>Campground: {{ booking.campgroundName }}</p>
         <p>Check-in Date: {{ booking.checkinDate }}</p>
         <p>Check-out Date: {{ booking.checkoutDate }}</p>
         <p>Zone: {{ booking.zoneName }}</p>
         <p>Nights: {{ booking.numberOfNights }}</p>
         <p>Name: {{ booking.name }}</p>
         <p>Email: {{ booking.email }}</p>
-        <p>Phone Number: ต้องการ</p>
+        <p>Phone Number: {{ booking.email }}</p>
         <p>Special Requests: {{ booking.specialRequests }}</p>
-        <p>Total Equipments Price: {{ booking.totalPrice }}</p>
+        <p>Total Price: {{ booking.totalPrice }}</p>
 
-        <p>Total Nights Price: {{ booking.totalPrice }}</p>
-
-        <p>All Total: {{ booking.totalPrice }}</p>
+        <img
+          :src="getCampgroundImage(booking.campgroundName)"
+          alt="Campground Image"
+          style="max-width: 100px; max-height: 100px"
+        />
 
         <button @click="deleteHandler(booking.id, index)">Delete</button>
       </div>
@@ -28,19 +31,25 @@
 <script setup>
 import { ref, onMounted } from "vue";
 import { useRouter } from "vue-router";
-import { fetchBookings, deleteBooking } from "../../libs/BookingFetch.js";
+import {
+  fetchBookings,
+  deleteBooking,
+  fetchCampgrounds,
+} from "../../libs/BookingFetch.js";
 
 const router = useRouter();
 const isLoading = ref(false);
 const bookings = ref([]);
+const campgrounds = ref([]);
 
 const fetchData = async () => {
   try {
     isLoading.value = true;
     bookings.value = await fetchBookings();
+    campgrounds.value = await fetchCampgrounds();
     isLoading.value = false;
   } catch (error) {
-    console.error("Error fetching history:", error);
+    console.error("Error fetching data:", error);
     isLoading.value = false;
   }
 };
@@ -61,8 +70,12 @@ const deleteHandler = async (id, index) => {
 const goToHomePage = () => {
   router.push("/");
 };
-</script>
 
+const getCampgroundImage = (campgroundName) => {
+  const campground = campgrounds.value.find((camp) => camp.name === campgroundName);
+  return campground && campground.image ? campground.image : "placeholder.jpg";
+};
+</script>
 <style scoped>
 .card {
   border: 1px solid #ccc;
